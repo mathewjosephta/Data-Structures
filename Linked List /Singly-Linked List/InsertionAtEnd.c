3efb19f123e7d007e5ae0fd2d6d479198b5a4952
Mathew Joseph t a <144910668+mathewjosephta@users.noreply.github.com>
2024-10-11 20:29:46 +0530

    Update InsertionAtEnd.c

--- Linked List /Singly-Linked List/InsertionAtEnd.c
+++ Linked List /Singly-Linked List/InsertionAtEnd.c
@@ -7,18 +7,13 @@ struct Node {
     struct Node* next;
 };
 
-// Function to create a new node
-struct Node* createNode(int value) {
+// Function to insert a new node at the end of the list
+void insertAtEnd(struct Node** head, int value) {
+    // Create a new node
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     newNode->data = value;
     newNode->next = NULL;
-    return newNode;
-}
 
-// Function to insert a node at the end of the linked list
-void insertAtEnd(struct Node** head, int value) {
-    struct Node* newNode = createNode(value);
-    
     // If the list is empty, make the new node the head
     if (*head == NULL) {
         *head = newNode;
@@ -31,11 +26,11 @@ void insertAtEnd(struct Node** head, int value) {
         temp = temp->next;
     }
 
-    // Insert the new node at the end
+    // Add the new node at the end
     temp->next = newNode;
 }
 
-// Function to display the linked list
+// Function to display the list
 void displayList(struct Node* head) {
     struct Node* temp = head;
     while (temp != NULL) {
@@ -49,28 +44,17 @@ int main() {
     struct Node* head = NULL;
     int n, value;
 
-    printf("Enter the number of elements to insert: ");
+    printf("How many numbers do you want to insert? ");
     scanf("%d", &n);
 
     for (int i = 0; i < n; i++) {
-        printf("Enter value for node %d: ", i+1);
+        printf("Enter value for node %d: ", i + 1);
         scanf("%d", &value);
         insertAtEnd(&head, value);
     }
 
-    printf("Linked List: ");
+    printf("The linked list is: ");
     displayList(head);
 
     return 0;
 }
-
-//SAMPLE OUTPUT
-/*
-Enter the number of elements to insert: 4
-Enter value for node 1: 10
-Enter value for node 2: 20
-Enter value for node 3: 30
-Enter value for node 4: 40
-Linked List: 10 -> 20 -> 30 -> 40 -> NULL
-
-*/
