26ee182df82f8486a82b741c53629763842a6217
Mathew Joseph T A <mathewjoseph3831v@gmail.com>
2024-12-22 08:22:53 +0530

    Create detect_remove_loop.c

---
+++ Linked List /Singly-Linked List/Problems /detect_remove_loop.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void detectAndRemoveLoop(Node* head) {
+    Node* slow = head;
+    Node* fast = head;
+
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast) {
+            break;
+        }
+    }
+
+    if (slow == fast) {
+        slow = head;
+
+        if (slow == fast) {
+            while (fast->next != slow) {
+                fast = fast->next;
+            }
+        } else {
+            while (slow->next != fast->next) {
+                slow = slow->next;
+                fast = fast->next;
+            }
+        }
+
+        fast->next = NULL;
+    }
+}
+
+void printList(Node* head) {
+    while (head) {
+        printf("%d -> ", head->data);
+        head = head->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    Node* head = createNode(1);
+    head->next = createNode(2);
+    head->next->next = createNode(3);
+    head->next->next->next = createNode(4);
+    head->next->next->next->next = head->next;
+
+    detectAndRemoveLoop(head);
+
+    printf("Linked List after removing loop: ");
+    printList(head);
+
+    return 0;
+}
