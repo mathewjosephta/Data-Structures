41acbf6ed82520ec2604f40eb7385f707927b39a
Mathew Joseph T A <mathewjoseph3831v@gmail.com>
2024-12-22 07:49:22 +0530

    Create remove_duplicates_sorted.c

---
+++ Linked List /Singly-Linked List/Problems /remove_duplicates_sorted.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void removeDuplicates(Node* head) {
+    Node* current = head;
+
+    while (current && current->next) {
+        if (current->data == current->next->data) {
+            Node* temp = current->next;
+            current->next = current->next->next;
+            free(temp);
+        } else {
+            current = current->next;
+        }
+    }
+}
+
+void printList(Node* head) {
+    while (head) {
+        printf("%d -> ", head->data);
+        head = head->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    Node* head = createNode(1);
+    head->next = createNode(1);
+    head->next->next = createNode(2);
+    head->next->next->next = createNode(3);
+    head->next->next->next->next = createNode(3);
+
+    printf("Original List: ");
+    printList(head);
+
+    removeDuplicates(head);
+    printf("List after removing duplicates: ");
+    printList(head);
+
+    return 0;
+}
