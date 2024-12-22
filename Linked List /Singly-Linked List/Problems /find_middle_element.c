f29705aaed539315fb9e5018fc59aaf68b312268
Mathew Joseph T A <mathewjoseph3831v@gmail.com>
2024-12-22 07:49:01 +0530

    Create find_middle_element.c

---
+++ Linked List /Singly-Linked List/Problems /find_middle_element.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void findMiddle(Node* head) {
+    Node* slow = head;
+    Node* fast = head;
+
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    if (slow) {
+        printf("Middle element: %d\n", slow->data);
+    }
+}
+
+int main() {
+    Node* head = createNode(1);
+    head->next = createNode(2);
+    head->next->next = createNode(3);
+    head->next->next->next = createNode(4);
+    head->next->next->next->next = createNode(5);
+
+    findMiddle(head);
+
+    return 0;
+}
